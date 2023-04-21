50e2846344da1b68c58c833ab7eab448ceac3500
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-04-21 11:36:16 +0530

    Create empty_check.py

---
+++ empty_check.py
@@ -0,0 +1,11 @@
+def Enquiry(lis1):
+    if len(lis1) == 0:
+        return 0
+    else:
+        return 1
+ 
+lis1 = []
+if Enquiry(lis1):
+    print("The list is not empty")
+else:
+    print("Empty List")
